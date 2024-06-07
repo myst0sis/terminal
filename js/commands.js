cc45a79e2e646cdb0cf9e76358655632a9c44777
unknown <mystosis03@gmail.com>
2024-06-07 19:14:49 +1000

    first commit, totally didnt forget

---
+++ js/commands.js
@@ -0,0 +1,73 @@
+var youtube = "https://www.youtube.com/@mystosis/";
+var twitter = "https://www.twitter.com/mystosis_/";
+var twitch = "https://www.twitch.tv/mystosis_";
+var password = "fkcodes";
+
+whois = [
+  "<br>",
+  "Hi, I'm Myst!",
+  "I make silly little drawings and play silly little characters on the internet.",
+  "Sometimes i'll pull something random out of my ass like this website.",
+  "Have fun exploring! Lots of things to find here :>",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "I don't know. Who are you? Why are you here?",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/mystosis' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/mystosis_' + '</a>',
+  'twitch       <a href="' + twitch + '" target="_blank">  twitch/mystosis_' + "</a>",
+  'consistency is key (･ω<) ☆',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "x1 TSMP Animatic                    10%",
+  "x1 Character reference sheet        85%",
+  "x1 Persona reference sheet          62%",
+  "x1 Minecraft model                  37%",
+  "<br>"
+];
+
+characters = [
+  "<br>",
+  '<span class="command">Persona</span>           Myst',
+  '<span class="command">Hiraeth</span>           Reo',
+  '<span class="command">Mirage</span>            Reo (Origins)',
+  '<span class="command">Venus</span>             Myst (Origins)',
+  '<span class="command">Tortuguita</span>        Myst',
+  '<span class="command">Tortuguita2</span>       Myst (Season 2)',
+  "<br>"
+]
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>           Who am I (Myst)?',
+  '<span class="command">whoami</span>          Who are you?',
+  '<span class="command">social</span>          Socials list',
+  '<span class="command">projects</span>        Current projects (WIPs)',
+  '<span class="command">characters</span>      My characters',
+  '<span class="command">help</span>            This menu',
+  '<span class="command">clear</span>           Clear terminal',
+  '<span class="command">smile</span>           What is this?',
+  "<br>",
+];
+
+banner = [
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+persona = [];
+hiraeth = [];
+mirage = [];
+venus = [];
+tortuguita = [];
+tortuguita2 = [];
